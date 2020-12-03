884bb87abb77332d97fbaceaabf27304026d3448
Will Nilges <wdn5796@rit.edu>
2020-12-03 08:36:01 -0500

    Generify ignite.sh

--- scripts/ignite.sh
+++ scripts/ignite.sh
@@ -3,17 +3,19 @@
 # Use ydotool to type in the ignition information. Screw doing that by hand!
 
 NODE_TYPE=bootstrap # bootstrap, or master, or worker
-NODE_TYPE=master
-NODE_TYPE=worker
-NODE_IP=10.10.33.76
+#NODE_TYPE=master
+#NODE_TYPE=worker
+NODE_IP= # If you don't have Static DHCP
 
-SERVICES=129.21.49.25
-NAMESERVER=10.10.33.70
-NODE_ROUTE=10.10.33.1
+SERVICES= # This must be specified.
+NAMESERVER=
+NODE_ROUTE=
 NODE_MASK=255.255.255.0
 
+# For static DHCP
 IGNITION=" coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://$SERVICES:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://$SERVICES:8080/okd4/$NODE_TYPE.ign"
 
+# For non-static DHCP
 #IGNITION=" ip=$NODE_IP::$NODE_ROUTE:$NODE_MASK:::none nameserver=$NAMESERVER coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://$SERVICES:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://$SERVICES:8080/okd4/$NODE_TYPE.ign"
 
 sleep 3
