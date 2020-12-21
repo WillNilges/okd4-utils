c3e7912d8004582021b564cd2cf16e9ca103ab5b
Will Nilges <wdn5796@rit.edu>
2020-12-20 20:56:09 -0500

    Clarify what variables must be filled

--- scripts/ignite.sh
+++ scripts/ignite.sh
@@ -2,15 +2,15 @@
 
 # Use ydotool to type in the ignition information. Screw doing that by hand!
 
-#NODE_TYPE=bootstrap # bootstrap, or master, or worker
-NODE_TYPE=master
-#NODE_TYPE=worker
-NODE_IP= # If you don't have Static DHCP
+# If you use static DHCP, fill out only these fields
+NODE_TYPE= # bootstrap, or master, or worker
+SERVICES= # IP address of your services VM
 
-SERVICES= # This must be specified.
+# If you don't use static DHCP, fill these out as well
+NODE_IP=
 NAMESERVER=
 NODE_ROUTE=
-NODE_MASK=255.255.255.0
+NODE_MASK=
 
 # For static DHCP
 IGNITION=" coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://$SERVICES:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://$SERVICES:8080/okd4/$NODE_TYPE.ign"
@@ -18,5 +18,6 @@ IGNITION=" coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://$SERVIC
 # For non-static DHCP
 #IGNITION=" ip=$NODE_IP::$NODE_ROUTE:$NODE_MASK:::none nameserver=$NAMESERVER coreos.inst.install_dev=/dev/sda coreos.inst.image_url=http://$SERVICES:8080/okd4/fcos.raw.xz coreos.inst.ignition_url=http://$SERVICES:8080/okd4/$NODE_TYPE.ign"
 
+echo 'Igniting in 3 seconds...'
 sleep 3
 ydotool type "$IGNITION"
